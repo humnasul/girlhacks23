dde446e575da6e34f54873f2d89e6575197af9cc
Humna <humnasultan@gmail.com>
2023-09-23 19:48:52 -0400

    Update app_test.py

--- app_test.py
+++ app_test.py
@@ -1,3 +1,10 @@
 import streamlit as st
 
-st.set_page_config(page_title="testing webpage", page_icon=":tada:", layout="wide");
+st.set_page_config(page_title="testing webpage", page_icon=":tada:", layout="wide")
+
+st.subheader("testing")
+
+label = "cat"
+
+st.button(label, key=None, help=None, on_click=None, args=None, kwargs=None, type="secondary", disabled=False, use_container_width=False)
+st.button(label, key=None, help=None, on_click=None, args=None, kwargs=None, type="secondary", disabled=False, use_container_width=False)
