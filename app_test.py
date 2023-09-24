4350b9d9cb797567f7b037b55090a59bd5346e83
Humna <humnasultan@gmail.com>
2023-09-23 20:17:12 -0400

    Update app_test.py

--- app_test.py
+++ app_test.py
@@ -4,7 +4,15 @@ st.set_page_config(page_title="testing webpage", page_icon=":tada:", layout="wid
 
 st.subheader("testing")
 
-label = "cat"
+label1 = "button1"
+label2 = "button2"
+label3 = "button3"
 
-st.button(label, key=None, help=None, on_click=None, args=None, kwargs=None, type="secondary", disabled=False, use_container_width=False)
-st.button(label, key=None, help=None, on_click=None, args=None, kwargs=None, type="secondary", disabled=False, use_container_width=False)
+col1, col2, col3 = st.columns([1,1,1])
+
+with col1:
+    st.button(label1, key="01", help=None, on_click=None, args=None, kwargs=None, type="secondary", disabled=False, use_container_width=False)
+with col2:
+    st.button(label2, key="02", help=None, on_click=None, args=None, kwargs=None, type="secondary", disabled=False, use_container_width=False)
+with col3:
+    st.button(label3, key="03", help=None, on_click=None, args=None, kwargs=None, type="secondary", disabled=False, use_container_width=False)
