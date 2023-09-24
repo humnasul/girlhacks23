a20a0a97661786b4512f6d4a70b59d7576d4b554
Humna <humnasultan@gmail.com>
2023-09-23 21:25:37 -0400

    Update app_test.py

--- app_test.py
+++ app_test.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from streamlit_modal import Modal
 
 st.set_page_config(page_title="testing webpage", page_icon=":tada:", layout="wide")
 
@@ -14,17 +15,44 @@ label6 = "button6"
 col1, col2, col3 = st.columns([1,1,1])
 
 with col1:
-    st.button(label1, key="01", help=None, on_click=None, args=None, kwargs=None, type="secondary", disabled=False, use_container_width=False)
+    but1 = st.button(label1, key="01", help=None, on_click=None, args=None, kwargs=None, type="secondary", disabled=False, use_container_width=False)
 with col2:
-    st.button(label2, key="02", help=None, on_click=None, args=None, kwargs=None, type="secondary", disabled=False, use_container_width=False)
+    but2 = st.button(label2, key="02", help=None, on_click=None, args=None, kwargs=None, type="secondary", disabled=False, use_container_width=False)
 with col3:
-    st.button(label3, key="03", help=None, on_click=None, args=None, kwargs=None, type="secondary", disabled=False, use_container_width=False)
+    but3 = st.button(label3, key="03", help=None, on_click=None, args=None, kwargs=None, type="secondary", disabled=False, use_container_width=False)
 
 col1, col2, col3 = st.columns([2,2,2])
 
 with col1:
-    st.button(label4, key="04", help=None, on_click=None, args=None, kwargs=None, type="secondary", disabled=False, use_container_width=False)
+    but4 = st.button(label4, key="04", help=None, on_click=None, args=None, kwargs=None, type="secondary", disabled=False, use_container_width=False)
 with col2:
-    st.button(label5, key="05", help=None, on_click=None, args=None, kwargs=None, type="secondary", disabled=False, use_container_width=False)
+    but5 = st.button(label5, key="05", help=None, on_click=None, args=None, kwargs=None, type="secondary", disabled=False, use_container_width=False)
 with col3:
-    st.button(label6, key="06", help=None, on_click=None, args=None, kwargs=None, type="secondary", disabled=False, use_container_width=False)
+    but6 = st.button(label6, key="06", help=None, on_click=None, args=None, kwargs=None, type="secondary", disabled=False, use_container_width=False)
+
+modal = Modal(key="popup_text",title="button text")
+if but1:
+    with modal.container():
+        st.markdown(":black[one]")
+
+if but2:
+    with modal.container():
+        st.markdown('two')
+if but3:
+    with modal.container():
+        st.markdown('three')
+if but4:
+    with modal.container():
+        st.markdown('four')
+if but5:
+    with modal.container():
+        st.markdown('five')
+if but6:
+    with modal.container():
+        st.markdown('six')
+
+# Create some data
+data = [10, 20, 30]
+
+# Create a pie chart
+st.piechart(data)
